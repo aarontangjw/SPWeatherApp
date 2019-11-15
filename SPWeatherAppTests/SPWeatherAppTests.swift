d187fda0bd6a6a5f0dfe8ad8e9d0e132dd69a9de
Aaron Tang <aarontangjw@gmail.com>
2019-11-15 15:57:50 +0800

    Commit Application Tests.

--- SPWeatherAppTests/SPWeatherAppTests.swift
+++ SPWeatherAppTests/SPWeatherAppTests.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2019 Aaron Tang. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import SPWeatherApp
+
+
+class SPWeatherAppTests: XCTestCase {
+    let homeSearchVM = searchViewModel()
+
+    func testUserViewedLocations(){
+        try! homeSearchVM.getUserLocations()
+        let location : Location = Location()
+        
+        location.areaName = "New York"
+        location.country = "New York"
+        location.latitude = "40.7128"
+        location.longitude = "74.0060"
+        location.region = ""
+        location.weatherUrl = ""
+        
+       //XCTAssertEqual(homeSearchVM.locations[0], location, "the array should be sorted properly")
+
+
+    }
+    
+    func testSearchLocation(){
+        //call API
+        homeSearchVM.searchLocation("New York", { (success, result, error) -> Void in
+            if(success) {
+                self.homeSearchVM.locations.removeAll()
+                if(result.count > 0){
+                    self.homeSearchVM.locations = result
+                }
+                
+                XCTAssertNil(result)
+                XCTAssert(self.homeSearchVM.locations[0].areaName == "New York")
+                XCTAssert(self.homeSearchVM.locations[0].country == "New York")
+            }
+        })
+    }
+}
